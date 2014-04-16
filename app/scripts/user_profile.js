bbe1e1eb35f386e73eae0142325169c5b56c181a
eyueld-bloc <eyueld+bloc@gmail.com>
2014-04-16 23:23:24 +0000

    made changes to collection.scss.Started on user page

--- app/scripts/user_profile.js
+++ app/scripts/user_profile.js
@@ -1,5 +1,13 @@
+var yourAccount = function(event){
+  $(".subscription").css('display','none');
+}
+
 
 if (document.URL.match(/\/user/)) {
   $(document).ready(function() {
+
+     $(".your-account").mousedown(function(){
+         $(".subscription").addClass("subscription-remove");
+     });
   });
 }
