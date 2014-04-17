18a82212a0e762e5c483da67226c2a348b01f9d7
eyueld-bloc <eyueld+bloc@gmail.com>
2014-04-17 23:39:39 +0000

    Fixed all issues with /user page. Fnished with the scss and js respective files

--- app/scripts/user_profile.js
+++ app/scripts/user_profile.js
@@ -1,13 +1,67 @@
-var yourAccount = function(event){
-  $(".subscription").css('display','none');
+
+var creditCard1 = {
+  cCardType: 'Visa',
+  cCardNumber: 'xxxx-1234',
+}
+var creditCard2 = {
+  cCardType: 'Master Card',
+  cCardNumber: 'xxxx-5678',
 }
 
+var createCardRow = function(cCardType, cCardNumber) {
+
+  var template = 
+        '<tr class = "credit-card-information" >'
+      + ' <td class="col-md-3" >' + cCardType  + '</td>'
+      + ' <td class="col-md-7">' + cCardNumber + '</td>'
+      + ' <td class="col-md-2">' + '<i class="fa fa-minus-circle"></i>' + '</td>'
+      + '</tr>'
+      ;
+
+  return $(template);
+};
+
+var displayCCardInfo = function(CCard){
+    var $cCardList = $('.creditCard-list-table');
+    console.log($cCardList.length)
+    $cCardList.empty();
+
+    for(var i = 0; i < CCard.length; i++){
+      var cCardData = CCard[i];
+      var $newCCRow = createCardRow(cCardData.cCardType, cCardData.cCardNumber);
+      $cCardList.append($newCCRow);
+    }
+}
+
+var showTab = function(containerName){
+  $("."+ containerName).removeClass("hidden");
+}
+var hideTab = function(containerName){
+  $("."+ containerName).addClass("hidden");
+}
+
+var CCard = [creditCard1, creditCard2];
 
 if (document.URL.match(/\/user/)) {
   $(document).ready(function() {
 
-     $(".your-account").mousedown(function(){
-         $(".subscription").addClass("subscription-remove");
+     $(".your-account").click(function(event){
+         showTab($(this).attr('data-tab-container'));
+         hideTab($(".subscription").attr('data-tab-container'));
+         $(this).parent().addClass("active");
+         $(".subscription").parent().removeClass("active");
+         event.preventDefault();
      });
+     $(".subscription").click(function(event){
+         showTab($(this).attr('data-tab-container'));
+         hideTab($(".your-account").attr('data-tab-container'));
+         $(this).parent().addClass("active");
+         $(".your-account").parent().removeClass("active");
+         displayCCardInfo(CCard);
+         event.preventDefault();
+     });
+    showTab('container-personal-information');
+    hideTab('container-subscription-page');
+    $(".your-account").parent().addClass("active");
   });
 }
